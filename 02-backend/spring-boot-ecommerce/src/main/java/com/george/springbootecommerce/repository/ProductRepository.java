d55992a4dabed4e2ec9a7e26d7fd8c6f08753e7e
Costi <costi_georgescu2000@yahho.com>
2023-02-02 08:44:46 +0200

    first commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/george/springbootecommerce/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.george.springbootecommerce.repository;
+
+import com.george.springbootecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
